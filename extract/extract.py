cec188a3d679d308469df590c4936966e75199b3
Aubin65 <amorlas65@gmail.com>
2024-11-05 16:23:55 +0100

    Ajout fonction dans la partie extract

--- extract/extract.py
+++ extract/extract.py
@@ -1,3 +1,30 @@
 """
 Cette partie est utilisée pour extraire la donnée depuis le site de la FFR
 """
+
+import luigi
+
+class ExtractJoueur(luigi.Task):
+    """Tâche permettant l'acquisition d'une équipe au format 
+    compositions = {
+        nom_equipe_1: {
+            "1" : Initiale. Nom,
+            "2" : Initiale. Nom,
+            ...
+        }
+        nom_equipe_2: {
+            "1" : Initiale. Nom,
+            "2" : Initiale. Nom,
+            ...
+        }
+    }
+    """
+
+    def output(self):
+        """
+        Cette fonction renvoie une 
+        """
+        return luigi.LocalTarget("transformed_data.csv")
+
+    def run(self):
+        
