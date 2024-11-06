96cde23d367106ddac400be5355d57ff016539a6
Aubin65 <amorlas65@gmail.com>
2024-11-06 16:24:42 +0100

    Retrait de l'erreur inutile

--- luigi/main.py
+++ luigi/main.py
@@ -1,5 +1,5 @@
 import luigi
-from transform.code.transform import TransformJoueur
+from transform.code.transform import TransformJoueur  # noqa
 
 if __name__ == "__main__":
     luigi.run(["--module", "transform.code.transform", "TransformJoueur", "--local-scheduler"])
