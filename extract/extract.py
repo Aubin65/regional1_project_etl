00a19b921b91a8679334be8fe5b391554b7aafc1
Aubin65 <amorlas65@gmail.com>
2024-11-06 09:45:44 +0100

    Changements avec Linter plus Formatter sur le fichier extract.py

--- extract/extract.py
+++ extract/extract.py
@@ -1,9 +1,8 @@
 """
 Cette partie est utilisée pour extraire la donnée depuis le site de la FFR
 """
+
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 import time
@@ -11,10 +10,10 @@ from selenium.webdriver.chrome.options import Options
 from pprint import pprint
 import luigi
 import json
-import pandas
+
 
 class ExtractJoueur(luigi.Task):
-    """Tâche permettant l'acquisition d'une équipe au format 
+    """Tâche permettant l'acquisition d'une équipe au format
     compositions = {
         nom_equipe_1: {
             "1" : Initiale. Nom,
@@ -40,9 +39,8 @@ class ExtractJoueur(luigi.Task):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
 
-
         # Configuration de Selenium avec ChromeDriver
-        service = ChromeService(executable_path='/usr/bin/chromedriver')
+        service = ChromeService(executable_path="/usr/bin/chromedriver")
         driver = webdriver.Chrome(service=service, options=chrome_options)
 
         # Remplacez par l'URL que vous souhaitez visiter
@@ -58,9 +56,11 @@ class ExtractJoueur(luigi.Task):
         equipe_1, equipe_2 = tab_selector[1].text.strip().split("\n")
         print(f"Equipe 1 : {equipe_1}\nEquipe 2 : {equipe_2}")
 
-        #Extraire la composition
+        # Extraire la composition
         try:
-            composition = {f"{equipe_1}" : {f"{i}" : driver.find_element(By.ID, f'poste_{i}').text.strip() for i in range(1,23)}}
+            composition = {
+                f"{equipe_1}": {f"{i}": driver.find_element(By.ID, f"poste_{i}").text.strip() for i in range(1, 23)}
+            }
             pprint(composition)
         except Exception as e:
             print("Joueur non trouvé", e)
@@ -69,15 +69,16 @@ class ExtractJoueur(luigi.Task):
         driver.execute_script("arguments[0].scrollIntoView(true);", button)
         button.click()
 
-        #Extraire la composition
+        # Extraire la composition
         try:
-            composition = {f"{equipe_2}" : {f"{i}" : driver.find_element(By.ID, f'poste_{i}').text.strip() for i in range(1,23)}}
+            composition = {
+                f"{equipe_2}": {f"{i}": driver.find_element(By.ID, f"poste_{i}").text.strip() for i in range(1, 23)}
+            }
             pprint(composition)
         except Exception as e:
             print("Joueur non trouvé", e)
 
-        with self.output().open('w') as f:
+        with self.output().open("w") as f:
             json.dump(composition, f, indent=4)
 
         driver.quit()
-
